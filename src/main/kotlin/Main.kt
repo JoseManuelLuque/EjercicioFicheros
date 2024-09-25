4eceff29e37a77d451d001cbbb317b35276d4789
José Manuel Luque <jluqgon214@g.educaand.es>
2024-09-25 15:51:11 +0200

    Test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,18 @@
-/**
- * EJERCICIO FICHEROS
- *
- * El fichero cotizacion.csv (que podéis encontrar en la carpeta ficheros) contiene las cotizaciones de las empresas del IBEX35 con las siguientes columnas: Nombre (nombre de la empresa), Final (precio de la acción al cierre de bolsa), Máximo (precio máximo de la acción durante la jornada), Mínimo (precio mínimo de la acción durante la jornada), Volumen (Volumen al cierre de bolsa), Efectivo (capitalización al cierre en miles de euros).
- *
- * Construir una función reciba el fichero de cotizaciones y devuelva un diccionario con los datos del fichero por columnas.
- *
- * Construir una función que reciba el diccionario devuelto por la función anterior y cree un fichero en formato csv con el mínimo, el máximo y la media de dada columna.
- */
-fun main() {
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
 
+fun main() {
+    val rutaFichero = Path.of("src").resolve("main").resolve("resources").resolve("cotizacion.csv")
+    val fichero = File("src/main/resources/cotizacion.csv")
+    crearDiccionario(rutaFichero, fichero)
 }
+
+fun crearDiccionario(rutaArchivo: Path, archivo: File) {
+    Files.newBufferedWriter(rutaArchivo)
+}
+
+//fun crearCsv(diccionario: ){
+//
+//
+//}
