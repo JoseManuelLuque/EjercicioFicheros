8dc5b34edf02a441ebf652696a08832567882d48
José Manuel Luque <jluqgon214@g.educaand.es>
2024-09-26 16:23:52 +0200

    Checkpoint

--- src/main/kotlin/GestionFichero.kt
+++ src/main/kotlin/GestionFichero.kt
@@ -12,7 +12,23 @@ class GestionFichero() {
         var listaSpliteada = mutableListOf<List<String>>()
         listaEmpresas.forEach {linea -> listaSpliteada.add(linea.split(";"))}
 
-        println(listaSpliteada)
+        //Lista de listas
+        var listaNombres = mutableMapOf<String, String>()
+
+        var titulos = mutableListOf<String>()
+        var empresas = mutableListOf<MutableList<String>>()
+        for (i in listaEmpresas) {
+            println(i.split(";").toString())
+            if (i.toInt() == 0){
+                titulos.add(i.split(";").toString())
+            }
+            else {
+                println("test")
+            }
+        }
+
+        println(titulos)
+        println(empresas)
     }
 
     fun crearCsv(){
