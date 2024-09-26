5b61f55bdd421d0d2e6d69e583a8c8de89a91ca8
José Manuel Luque <jluqgon214@g.educaand.es>
2024-09-27 00:48:24 +0200

    Reset

--- src/main/kotlin/GestionFichero.kt
+++ src/main/kotlin/GestionFichero.kt
@@ -5,30 +5,14 @@ import java.nio.file.Path
 class GestionFichero() {
 
     fun crearDiccionario(rutaArchivo: Path, archivo: File) {
-        //Lista que reune en cada posicion toodos los datos de cada empresa
-        var listaEmpresas = mutableListOf<String>()
-        Files.newBufferedReader(rutaArchivo).use { stream -> stream.forEachLine { listaEmpresas.add(it) } }
+        var Diccionario = mutableMapOf<String, String>()
 
-        var listaSpliteada = mutableListOf<List<String>>()
-        listaEmpresas.forEach {linea -> listaSpliteada.add(linea.split(";"))}
-
-        //Lista de listas
-        var listaNombres = mutableMapOf<String, String>()
-
-        var titulos = mutableListOf<String>()
-        var empresas = mutableListOf<MutableList<String>>()
-        for (i in listaEmpresas) {
-            println(i.split(";").toString())
-            if (i.toInt() == 0){
-                titulos.add(i.split(";").toString())
-            }
-            else {
-                println("test")
+        Files.newBufferedReader(rutaArchivo).use {
+            stream -> stream.forEachLine {
+                line -> println(line)
             }
         }
 
-        println(titulos)
-        println(empresas)
     }
 
     fun crearCsv(){
